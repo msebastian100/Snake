a7dd916d3d263cce5bc947bfe51b69528f1dc31c
basti [msebastian100] <basti-unterwegs@mallach.de>
2022-08-03 15:47:17 +0200

    upd

--- snake.py
+++ snake.py
@@ -193,16 +193,16 @@ while True:
 		if event.type == SCREEN_UPDATE:
 			main_game.update()
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP:
+			if event.key == pygame.K_w:
 				if main_game.snake.direction.y != 1:
 					main_game.snake.direction = Vector2(0,-1)
-			if event.key == pygame.K_RIGHT:
+			if event.key == pygame.K_d:
 				if main_game.snake.direction.x != -1:
 					main_game.snake.direction = Vector2(1,0)
-			if event.key == pygame.K_DOWN:
+			if event.key == pygame.K_s:
 				if main_game.snake.direction.y != -1:
 					main_game.snake.direction = Vector2(0,1)
-			if event.key == pygame.K_LEFT:
+			if event.key == pygame.K_a:
 				if main_game.snake.direction.x != 1:
 					main_game.snake.direction = Vector2(-1,0)
 
